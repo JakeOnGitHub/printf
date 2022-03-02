336e312a2bd483f3c9bff2161599805c38420704
jarrettjewart <92384372+jarrettjewart@users.noreply.github.com>
2022-03-02 15:03:00 -0600

    Create main.h
    MAin.h file update

---
+++ main.h
@@ -0,0 +1,16 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stido.h>
+#include <stdlib.h>
+#include <stdf.h>
+#include <limits.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+
+#endif /* MAIN_H */
+~                  
