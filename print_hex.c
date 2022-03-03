4c4b89f2915d61af406852f3aba8490761f82162
jarrettjewart <92384372+jarrettjewart@users.noreply.github.com>
2022-03-03 09:29:22 -0600

    Create print_hex.c
    Print hex function

---
+++ print_hex.c
@@ -0,0 +1,25 @@
+#include "main.h"
+  
+/**
+ * hex_print - prints hex in ascii value
+ * @c: char
+ * Return: count
+ */
+
+ctatic int hex_print(char c)
+{
+        int count;
+        char diff = 'A' - ':';
+        char d[2];
+
+        d[0] = c / 16;
+        d[1] = c % 16;
+        for (count = 0; count < 2; count++)
+        {
+                if (d[count] >= 10)
+                        _putchar('0' + diff + d[count]);
+                else
+                        _putchar('0' + d[count]);
+        }
+        return (count);
+}
