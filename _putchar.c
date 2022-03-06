dcb7c514edfbf6cf2d66af0d1946a635dfefbc52
jakeongithub <jakecrandall@gmail.com>
2022-03-06 10:32:38 -0800

    cleaned up putchar commit

--- _putchar.c
+++ _putchar.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <unistd.h>
 
+/**
+* _putchar - entry point.  prints char
+* @c: char being evaluated
+* Return: Always 0
+*/
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
